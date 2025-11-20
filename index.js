8788fb5d3bd5f097cddf8610abcb1318ea215cff
dwege <ojorodavid@gmail.com>
2025-11-20 10:43:08 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const temperature = 90;
+const timeOfDay = "morning";
+let soilMoisture = 30;
+if (temperature > 80) {
+    console.log = "watering on"
+} else {
+    console.log = "watering off"
+}
+console.log(45)
